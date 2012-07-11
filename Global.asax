04cc3ccaba755fd3259e5c7a5495db7aa4bd72b0
MuffunKevin <muffunkevin@gmail.com>
2012-07-10 21:51:24 -0400

    Initial commit
    Hello GitHub

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CallLoger.MvcApplication" Language="C#" %>
---
+++ Models/CallType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallLoger.Models
+{
+    public class CallType
+    {
+        public int ID { get; set; }
+    }
+}
